5f87270cb5abc232946bafc2ede76387f9c9393b
nrymarz <nrymarz@gmail.com>
2020-11-01 18:32:08 -0600

    commit tree a8b20517848f892b3109f4a8dc2911c0970a2e9d

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
-# require libraries/modules here
-
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2").css("strong").text.strip.to_sym
+    projects[title] = {}
+    projects[title][:image_link] = project.css('img').attribute("src").value
+    projects[title][:description] = project.css("p.bbcard_blurb").text.strip
+    projects[title][:location] = project.css("span.location-name").text
+    projects[title][:percent_funded] = project.css("li.first.funded strong").text.to_i
+  end
+  projects
 end
+
